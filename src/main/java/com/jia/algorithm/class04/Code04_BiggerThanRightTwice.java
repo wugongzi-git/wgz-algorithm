5b5b7d3c76d83f73119999c36df2db3276f70121
wugongzi-git <1346189649@qq.com>
2023-12-19 18:38:57 +0800

    [feat]- 逆序对

---
+++ src/main/java/com/jia/algorithm/class04/Code04_BiggerThanRightTwice.java
@@ -0,0 +1,118 @@
+package com.jia.algorithm.class04;
+
+//  本题测试链接 : https://leetcode.com/problems/reverse-pairs/
+
+/**
+ * 给定一个数组 nums ，如果 i < j 且 nums[i] > 2*nums[j] 我们就将 (i, j) 称作一个重要翻转对。
+ * 你需要返回给定数组中的重要翻转对的数量。
+ */
+public class Code04_BiggerThanRightTwice {
+    public static int reversePairs(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        return process(arr, 0, arr.length - 1);
+    }
+
+    public static int process(int[] arr, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        // l < r
+        int mid = l + ((r - l) >> 1);
+        return process(arr, l, mid) + process(arr, mid + 1, r) + merge(arr, l, mid, r);
+    }
+
+    public static int merge(int[] arr, int L, int M, int R) {
+        int res = 0;
+        int windowR = M+1; // 滑动窗口
+        for (int i = L; i <= M; i++) {
+            while (windowR <= R && arr[i] > arr[windowR] * 2) {
+                windowR++;
+            }
+            res += windowR - M - 1;
+        }
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (int j = 0; j < help.length; j++) {
+            arr[L + j] = help[j];
+        }
+        return res;
+    }
+
+    // for test
+    public static int comparator(int[] arr) {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > (arr[j] << 1)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 50000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            if (reversePairs(arr1) != comparator(arr2)) {
+                System.out.println("Oops!");
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+}
