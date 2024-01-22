6a5fcac45146e70b6c921b4531e9685e2acb3a6a
wugongzi-git <1346189649@qq.com>
2024-01-22 14:14:04 +0800

    [feat]- 二叉树按层打印

---
+++ src/main/java/com/jia/algorithm/class11/Code01_DeleteBTNode.java
@@ -0,0 +1,40 @@
+package com.jia.algorithm.class11;
+
+/**
+ * 删除二叉树中的节点
+ */
+public class Code01_DeleteBTNode {
+
+    public static class Node {
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node deleteNode(Node head, Node node) {
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/jia/algorithm/class11/Code01_LevelTraversalBT.java
@@ -0,0 +1,66 @@
+package com.jia.algorithm.class11;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 按层方式遍历二叉树
+ */
+public class Code01_LevelTraversalBT {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int v) {
+            value = v;
+        }
+    }
+
+    public static void level(Node head) {
+        if (head == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(head);
+        while (!queue.isEmpty()) {
+            // 弹出一个节点
+            Node poll = queue.poll();
+            System.out.print(poll.value + " ");
+            if (poll.left != null) {
+                queue.add(poll.left);
+            }
+            if (poll.right != null) {
+                queue.add(poll.right);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(9);
+        head.right = new Node(3);
+        head.left.left = new Node(12);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+        level(head);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
