3f15ae52e685ff7769cafae21876ac1b514d3d68
wugongzi-git <1346189649@qq.com>
2024-01-23 10:59:32 +0800

    [feat]- 二叉树序列化和反序列化

--- src/main/java/com/jia/algorithm/class11/Code01_LevelTraversalBT.java
+++ src/main/java/com/jia/algorithm/class11/Code01_LevelTraversalBT.java
@@ -38,29 +38,15 @@ public class Code01_LevelTraversalBT {
         }
     }
 
-
     public static void main(String[] args) {
         Node head = new Node(1);
-        head.left = new Node(9);
+        head.left = new Node(2);
         head.right = new Node(3);
-        head.left.left = new Node(12);
+        head.left.left = new Node(4);
         head.left.right = new Node(5);
         head.right.left = new Node(6);
         head.right.right = new Node(7);
         level(head);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
