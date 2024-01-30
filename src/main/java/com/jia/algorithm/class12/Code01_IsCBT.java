c883271ea2c744c0b112524a0d3b5b5d7da77112
wugongzi-git <1346189649@qq.com>
2024-01-30 12:55:36 +0800

    [feat]- 判断一棵树是否平衡二叉树

---
+++ src/main/java/com/jia/algorithm/class12/Code01_IsCBT.java
@@ -0,0 +1,2 @@
+package com.jia.algorithm.class12;public class Code01_IsCBT {
+}
---
+++ src/main/java/com/jia/algorithm/class12/Code02_IsBST.java
@@ -0,0 +1,143 @@
+package com.jia.algorithm.class12;
+
+import java.util.ArrayList;
+
+/**
+ * 是否平衡二叉树
+ */
+public class Code02_IsBST {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static boolean isBST1(Node head) {
+        if (head == null) {
+            return true;
+        }
+        ArrayList<Node> arr = new ArrayList<>();
+        in(head, arr);
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i).value <= arr.get(i - 1).value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 中序遍历二叉树，二叉树的值存入 ArrayList 中，二叉树中的值是按升序排列
+    public static void in(Node head, ArrayList<Node> arr) {
+        if (head == null) {
+            return;
+        }
+        in(head.left, arr);
+        arr.add(head);
+        in(head.right, arr);
+    }
+
+    /**
+     * 利用二叉树递归套路解法
+     */
+    public static boolean isBST2(Node head) {
+        if (head == null) {
+            return true;
+        }
+        return process(head).isBST;
+    }
+
+    /**
+     * 平衡二叉树需要找的信息
+     * - X 的左子树是否平
+     * - X 的右子树是否平
+     * - X 左右子树的高度
+     */
+    public static class Info {
+        public boolean isBST;
+        public int max;
+        public int min;
+
+        public Info(boolean isBST, int max, int min) {
+            this.isBST = isBST;
+            this.max = max;
+            this.min = min;
+        }
+    }
+
+    public static Info process(Node x) {
+        if (x == null) {
+            return null;
+        }
+        // 拿到左子树的信息
+        Info leftInfo = process(x.left);
+        // 拿到右子树的信息
+        Info rightInfo = process(x.right);
+        // 找最大值，最大值可能是左子树，也可能是右子树，因为右子树可能为 null
+        int max = x.value;
+        if (leftInfo != null) {
+            max = Math.max(max, leftInfo.max);
+        }
+        if (rightInfo != null) {
+            max = Math.max(max, rightInfo.max);
+        }
+        int min = x.value;
+        if (leftInfo != null) {
+            min = Math.min(min, leftInfo.min);
+        }
+        if (rightInfo != null) {
+            min = Math.min(min, rightInfo.min);
+        }
+        boolean isBST = true;
+        if (leftInfo != null && !leftInfo.isBST) {
+            isBST = false;
+        }
+        if (rightInfo != null && !rightInfo.isBST) {
+            isBST = false;
+        }
+        if (leftInfo != null && leftInfo.max >= x.value) {
+            isBST = false;
+        }
+        if (rightInfo != null && rightInfo.min <= x.value) {
+            isBST = false;
+        }
+        return new Info(isBST, max, min);
+    }
+
+
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 4;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isBST1(head) != isBST2(head)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
+
+
+
+}
